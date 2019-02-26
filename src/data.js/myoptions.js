79244a0964a7f332566791c902626c473c9bf8d2
Amin Khan <akhan@aacn.org>
2019-02-26 15:50:55 -0800

    ADD myoptions to DATA

---
+++ src/data.js/myoptions.js
@@ -0,0 +1,17 @@
+
+
+const myautoselect = [
+    "pear",
+    "apple",
+    "orange",
+    "strawberry",
+    "kiwi",
+    "grapes",
+    "melon",
+    "mango",
+    "passion"
+]
+
+const radioOptions = [
+    {value:"state 1"}
+]
