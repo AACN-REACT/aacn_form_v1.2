a46464ed2b9265697d460ab896d3fe179cebbd40
AKD <makart@live.co.uk>
2019-03-10 13:40:51 -0700

    EXTRACT ATTRIBUTES CORRECTLY FROM INDIVIDUAL FIELDS AND PASS TO GENERATEDMARKUP COMPONENT AS PROPS

--- src/utils/generate-markup.js
+++ src/utils/generate-markup.js
@@ -3,20 +3,22 @@ import checkField from './check-field.js'
 import writeToState from './write-to-state.js'
 import ElementContainer from '../components/molecules/element-container'
 
+const extractFields = function(finalArr) { return (finalArr.map(field=>[field[Object.keys(field)[0]].type,field[Object.keys(field)[0] ]]).
+map(type=>{console.log("SS", type); return typeof ElementContainer[type[0]]==="function"?
+ElementContainer[type[0]](type[1]):ElementContainer[type[0]]}) )}
 
 
 function generateMarkup (config){
     
 const formFields = config.fields;
 let Markup =[]
-let temp =  writeToState(formFields,Markup,true)
-console.log("TEsMPP", temp)
 let newArr= []
 let finalArr = writeToState(formFields,Markup,true);
 console.log("FinalArr",finalArr)
-finalArr = finalArr.map(field=>field[Object.keys(field)[0]].type).
-                    map(type=>{console.log("SS", type); return typeof ElementContainer[type]==="function"?
-                    ElementContainer[type]():ElementContainer[type]})
+finalArr = extractFields(finalArr)
+// finalArr = finalArr.map(field=>[field[Object.keys(field)[0]].type,field[Object.keys(field)[0] ]]).
+//                     map(type=>{console.log("SS", type); return typeof ElementContainer[type[0]]==="function"?
+//                     ElementContainer[type[0]](type[1]):ElementContainer[type[0]]})
                 //filter(component=>component!==undefined)
 console.log("Gnerate Markup" , finalArr)                
 return finalArr
