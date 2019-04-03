68dacbda4ea5b1ef624b83779c72f6477f40950e
AKD <makart@live.co.uk>
2019-04-03 00:30:09 -0700

    CORRECT STATE FOR INITIAL SELECT AND ON CHANGE EVENT

--- src/components/atoms/select-input.jsx
+++ src/components/atoms/select-input.jsx
@@ -4,11 +4,16 @@ import { FormPayload } from '../../utils/contexts/form-contexts';
 import { DispatchState} from '../../utils/contexts/form-contexts';
 
 const myref = React.createRef();
+
+
 export default function SelectInput(props){
    console.log("PPPROPS:" , props)
     let state = useContext(FormPayload)
     let dispatch = useContext(DispatchState)
     console.log("THIS IS A SELECT:", state, props.options, props.parent)
+
+
+
     function filterOptions( formstate, options, parent){
        console.log("parent formstatae:",formstate)
      if(parent){
@@ -23,6 +28,11 @@ export default function SelectInput(props){
 
     }
 
-    return <label>{props.label || props.thisfield}<select  onChange={e=>{let ans = props.options.filter(el=>el.name===e.target.value);dispatch(formAction("select",props.thisfield, [e.target.value, ans[0]['key']])) } }>{filterOptions(state,props.options,props.parent)}</select></label>
+    return <label>{props.label || props.thisfield}<select  
+    onChange={e=>{let ans = props.options.filter(el=>el.name===e.target.value);
+        dispatch(formAction("select",props.thisfield, [e.target.value, ans[0]['key']]));filterOptions(state,props.options,props.parent) } }>
+        {filterOptions(state,props.options,props.parent)}
+        </select>
+        </label>
 
 }
