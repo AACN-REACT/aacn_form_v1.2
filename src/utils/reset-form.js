7021b788f4f88bd4fe7ca40732164c03254db3fa
Amin Khan <akhan@aacn.org>
2019-04-23 15:49:42 -0700

    WORK ON RESET-FORM FUNCTION

--- src/utils/reset-form.js
+++ src/utils/reset-form.js
@@ -4,7 +4,7 @@ export default function resetForm(state) {
 let newstate = {}
 for(let i in state){
     console.log("iiiiii",state[i])
-       Array.isArray(state[i])?newstate[i]="":newstate[i]=state[i]
+       !Array.isArray(state[i])?newstate[i]="":newstate[i]=state[i]
 }
 return newstate
 }
